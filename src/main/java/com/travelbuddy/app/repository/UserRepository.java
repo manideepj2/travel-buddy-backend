dbec65f5b538477a443de378518c7e813ab90c24
Manideep Jammigumpula <manideepj2@gmail.com>
2026-03-17 18:23:49 -0400

    add exists check for database

--- src/main/java/com/travelbuddy/app/repository/UserRepository.java
+++ src/main/java/com/travelbuddy/app/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
